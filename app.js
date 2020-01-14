35200eef01a965feb0ea4692fab1306a0b90cc5b
student <student@Admins-iMac.local>
2020-01-14 14:27:11 -0600

    first commit

---
+++ app.js
@@ -0,0 +1,23 @@
+var myName = 'Rachel';
+// My name as a value.
+
+const states = 50; 
+// The 50 states.
+
+var number = 5 + 4;
+// Adding together.
+
+function sayHello(greeting) {
+  alert (greeting);
+}
+sayHello ("Hello world!");
+
+function checkAge(name, age) {
+
+}
+
+
+
+console.log(myName)
+console.log(states)
+console.log(number)
